896ee8a53a28463659c1dbe0f115a5992941f584
Illia Laziuk <illialaziuk2@gmail.com>
2021-02-19 18:12:10 +0300

    Frog eats mosquitoes

--- recursion/README.md
+++ recursion/README.md
@@ -9,3 +9,13 @@ Print that number MOD (10^9 + 7)\
 (Problem is how to count n! where n is very big)\
 Solution is to understand how to find ((a / b) mod c)\
 Then use formula ((a \* b) mod c) = (((a mod c) \* (b mod c)) mod c)\
+
+## frog.cpp - Max number of mosquitoes that frog can eat
+
+n - num of water lilies\
+next n numbers - number of mosquitoes in each lily\
+frog sits at the first lily\
+frog can jump 2 or 3 lilies ahead\
+in each lily it eats all the mosquitoes\
+find max num of mosquitoes that frog can eat\
+frog should be at the last lily after all jumps\
---
+++ recursion/frog.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int a, int b) {
+  return a - b > 0 ? a : b;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+
+  int n;
+  scanf("%d", &n);
+  if(n == 2) {
+    cout << -1;
+    return 0;
+  }
+
+  int* mosqLilies = new int[n + 1];
+  mosqLilies[0] = -2001;
+  scanf("%d", &mosqLilies[1]);
+  mosqLilies[2] = -2001;
+
+  int curMosq = 0;
+  int prev1 = 0;
+  int prev2 = 0;
+  scanf("%d", &prev1);
+
+  for(int i = 3; i <= n; i++) {
+    scanf("%d", &curMosq);
+    prev1 = mosqLilies[i - 2];
+    prev2 = mosqLilies[i - 3];
+    mosqLilies[i] = i - 2 == 2 ? prev2 + curMosq : i - 3 == 2 ? prev1 + curMosq : max(prev2 + curMosq, prev1 + curMosq);
+  }
+  cout << mosqLilies[n];
+}
